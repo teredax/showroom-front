86b7525f78f2b9e480994d61d943aa027d7ad734
Teredax <gonzisteel@gmail.com>
2020-03-18 17:43:50 -0600

    Cleaned up and removed unnecessary code

--- showroom/src/components/Header.vue
+++ showroom/src/components/Header.vue
@@ -1,27 +1,27 @@
 <template>
-  <header class="masthead" id="page-top">
+<header class="masthead" id="page-top">
     <div class="container">
-      <div class="intro-text">
-        <HeaderSearchbar></HeaderSearchbar>
-        <div class="intro-lead-in"></div>
-        <div class="intro-heading text-uppercase">
-          <HeaderData></HeaderData>
+        <div class="intro-text">
+            <HeaderSearchbar></HeaderSearchbar>
+            <div class="intro-lead-in"></div>
+            <div class="intro-heading text-uppercase">
+                <HeaderData></HeaderData>
+            </div>
         </div>
-      </div>
     </div>
-  </header>
+</header>
 </template>
-<script>
 
+<script>
 export default {
-     name: 'Header',
-  props: {
-    msg: String
-  },
-  components:{
-    HeaderSearchbar: () => import('@/components/Header/SearchBar'),
-    HeaderData: () => import('@/components/Header/Numbers'),
-  }
+    name: 'Header',
+    props: {
+        msg: String
+    },
+    components: {
+        HeaderSearchbar: () => import('@/components/Header/SearchBar'),
+        HeaderData: () => import('@/components/Header/Numbers'),
+    }
 }
 </script>
 
@@ -33,49 +33,51 @@ header.masthead {
     background-repeat: no-repeat;
     background-attachment: scroll;
     background-position: center center;
-    background-size:cover;
-  }
-  
-  header.masthead .intro-text {
+    background-size: cover;
+}
+
+header.masthead .intro-text {
     padding-top: 150px;
     padding-bottom: 10px;
-;  }
-  
-  header.masthead .intro-text .intro-lead-in {
+    ;
+}
+
+header.masthead .intro-text .intro-lead-in {
     font-size: 22px;
     font-style: italic;
     line-height: 22px;
     margin-bottom: 25px;
     font-family: 'Droid Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  
-  header.masthead .intro-text .intro-heading {
+}
+
+header.masthead .intro-text .intro-heading {
     font-size: 50px;
     font-weight: 700;
     line-height: 50px;
     margin-bottom: 0px;
     font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  
-  @media (min-width: 768px) {
+}
+
+@media (min-width: 768px) {
     header.masthead .intro-text {
-      padding-top: 150px;
-      padding-bottom: 10px;
+        padding-top: 150px;
+        padding-bottom: 10px;
     }
+
     header.masthead .intro-text .intro-lead-in {
-      font-size: 40px;
-      font-style: italic;
-      line-height: 40px;
-      margin-bottom: 25px;
-      font-family: 'Droid Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+        font-size: 40px;
+        font-style: italic;
+        line-height: 40px;
+        margin-bottom: 25px;
+        font-family: 'Droid Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
     }
+
     header.masthead .intro-text .intro-heading {
-      font-size: 75px;
-      font-weight: 700;
-      line-height: 75px;
-      margin-bottom: 0px;
-      font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+        font-size: 75px;
+        font-weight: 700;
+        line-height: 75px;
+        margin-bottom: 0px;
+        font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
     }
-  }
-  
+}
 </style>
--- showroom/src/components/NavBar.vue
+++ showroom/src/components/NavBar.vue
@@ -1,154 +1,161 @@
 <template>
-    <div class="topNavBar">
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav" >
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top" @click="resetItems()">
-        <img class ="logo-img" src="../assets/BPLogoTAG_RGB-72dpi_Ver01-Blanco.png">
-      </a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav text-uppercase ml-auto">
-          <li class="nav-item" v-for="(tag,i) in tags" :key="i">
-            <a class="nav-link js-scroll-trigger" href="#searchAnchor" @click="navGenerated(tag)">{{tag}}</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+<div class="topNavBar">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top" @click="resetItems()">
+                <img class="logo-img" src="../assets/BPLogoTAG_RGB-72dpi_Ver01-Blanco.png">
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto">
+                    <li class="nav-item" v-for="(tag,i) in tags" :key="i">
+                        <a class="nav-link js-scroll-trigger" href="#searchAnchor" @click="navGenerated(tag)">{{tag}}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
-      </div>
+</div>
 </template>
 
 <script>
-
-import {mapState} from 'vuex';
+import {
+    mapState
+} from 'vuex';
 
 export default {
-  name: 'NavBar',
-  mounted(){
-    window.addEventListener("scroll", function(){
-      const nav = document.querySelector('#mainNav');
-      if (this.scrollY > 80) {
-        nav.style.backgroundImage = "url(/img/wallpaper.57a95dba.jpg)";
-      }
-      else {
-        nav.style.backgroundImage = 'none';
-      // nav.style.backgroundColor= '#2c3e5094';
-      }
-    })
-  },
-  computed:mapState(['tags']),
+    name: 'NavBar',
+    mounted() {
+        window.addEventListener("scroll", function () {
+            const nav = document.querySelector('#mainNav');
+            if (this.scrollY > 80) {
+                nav.style.backgroundImage = "url(/img/wallpaper.57a95dba.jpg)";
+            } else {
+                nav.style.backgroundImage = 'none';
+            }
+        })
+    },
+    computed: mapState(['tags']),
 
-  methods:{
-    navGenerated(tag){
-      this.$store.dispatch('loadItemsByTag',tag)
+    methods: {
+        navGenerated(tag) {
+            this.$store.dispatch('loadItemsByTag', tag)
+        },
+        resetItems() {
+            this.$store.dispatch('loadItems')
+        },
     },
-    resetItems(){
-      this.$store.dispatch('loadItems')
-    },
-  },
 }
 </script>
 
 <style>
-  #app {
+#app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-  }
+}
 
-  #nav {
+#nav {
     padding: 5px;
-  }
-  nav.scroll {
+}
+
+nav.scroll {
     background-color: red;
-       height: 12%;
-  }
-  #nav a {
+    height: 12%;
+}
+
+#nav a {
     font-weight: bold;
     color: #2c3e50;
-  }
+}
 
-  #nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
     color: #42b983;
-  }
+}
 
-  #mainNav {
-  /* background-color: #2c3e5094; */
-     height: 12%;
-   
-  }
-  
-  #mainNav .navbar-toggler {
+#mainNav {
+    /* background-color: #2c3e5094; */
+    height: 12%;
+
+}
+
+#mainNav .navbar-toggler {
     font-size: 12px;
     right: 0;
     padding: 5px;
-   
+
     text-transform: uppercase;
     color: white;
     border: 0;
     background-color: #333366;
     font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  
-  #mainNav .navbar-brand {
+}
+
+#mainNav .navbar-brand {
     color: #0370fe;
     font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  
-  #mainNav .navbar-brand.active, #mainNav .navbar-brand:active, #mainNav .navbar-brand:focus, #mainNav .navbar-brand:hover {
+}
+
+#mainNav .navbar-brand.active,
+#mainNav .navbar-brand:active,
+#mainNav .navbar-brand:focus,
+#mainNav .navbar-brand:hover {
     color: #0370fe;
-  }
-  
-  #mainNav .navbar-nav .nav-item .nav-link {
+}
+
+#mainNav .navbar-nav .nav-item .nav-link {
     font-size: 90%;
     font-weight: 600;
     padding: 10px;
     letter-spacing: 1px;
     color: white;
     font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  
-  #mainNav .navbar-nav .nav-item .nav-link.active, #mainNav .navbar-nav .nav-item .nav-link:hover {
-    color: #333366;
-  }
+}
 
-  .logo-img{
+#mainNav .navbar-nav .nav-item .nav-link.active,
+#mainNav .navbar-nav .nav-item .nav-link:hover {
+    color: #333366;
+}
+
+.logo-img {
     display: flex;
     width: 120px;
     height: 70px;
-    
-  }
-  
-  @media (min-width: 992px) {
-    #mainNav {
-      padding-top: 25px;
-      padding-bottom: 25px;
-      transition: padding-top 0.3s, padding-bottom 0.3s;
-      border: none;
-     
-     
-    }
-    #mainNav .navbar-brand {
-      font-size: 1.75em;
-      transition: all 0.3s;
-    }
-    #mainNav .navbar-nav .nav-item .nav-link {
-      padding: 1.1em 1em !important;
-    }
-    #mainNav.navbar-shrink {
-      padding-top: 0;
-      padding-bottom: 0;
-    
-    }
-    #mainNav.navbar-shrink .navbar-brand {
-      font-size: 1.25em;
-      padding: 12px 0;
-    }
-  }
 
+}
+
+@media (min-width: 992px) {
+    #mainNav {
+        padding-top: 25px;
+        padding-bottom: 25px;
+        transition: padding-top 0.3s, padding-bottom 0.3s;
+        border: none;
+
+    }
+
+    #mainNav .navbar-brand {
+        font-size: 1.75em;
+        transition: all 0.3s;
+    }
+
+    #mainNav .navbar-nav .nav-item .nav-link {
+        padding: 1.1em 1em !important;
+    }
+
+    #mainNav.navbar-shrink {
+        padding-top: 0;
+        padding-bottom: 0;
+
+    }
+
+    #mainNav.navbar-shrink .navbar-brand {
+        font-size: 1.25em;
+        padding: 12px 0;
+    }
+}
 </style>
