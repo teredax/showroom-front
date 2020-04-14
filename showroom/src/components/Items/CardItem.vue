cc871a3cbbe5e65e67d14c61099682cf47836934
Jesus Lugo <lugoso96@hotmail.com>
2020-04-14 14:56:25 -0500

    CardItem Update

--- showroom/src/components/Items/CardItem.vue
+++ showroom/src/components/Items/CardItem.vue
@@ -1,161 +1,148 @@
 <template>
-
-
 <div class="container-fluid">
-  <div class="row rowCards" >
-    <div class="card-group col-12 nopadding" id="searchAnchor">
-      <div class="col-4 nopadding" v-for="(itemObj,i) in items" :key="`${i}-${itemObj.title}`">
-        <a class="card" data-toggle="modal" data-target=".bd-example-modal-lg" @click="displayDetails(itemObj._id)">
-          <img :src="itemObj.img_principal" class="card-img h-100" alt="...">
-            <div class="card-img-overlay">
-                <h1 class="card-title">{{itemObj.title}}</h1>
-                 <div class="overlay">
-                <p class="card-text">{{itemObj.title_descr_1}}</p>
-              </div>
-              <div class="badge-position">
-                  <ul v-for="(tag,j) in itemObj.tags" :key="j"> <a href="#" class="badge badge-pill badge-primary"> {{tag}} </a></ul>
-              </div>
-           
-            </div>
-        </a>
+    <div class="row rowCards">
+        <div class="card-group col-12 nopadding" id="searchAnchor">
+            <v-flex xs12 sm4 md4 v-for="(itemObj,i) in items" :key="`${i}-${itemObj.title}`">
+                <a class="card" data-toggle="modal" data-target=".bd-example-modal-lg" @click="displayDetails(itemObj._id)">
+                    <img :src="itemObj.img_principal" class="card-img h-100" alt="...">
+                    <div class="card-img-overlay">
+                        <h1 class="card-title">{{itemObj.title}}</h1>
+                        <div class="overlay">
+                            <p class="card-text">{{itemObj.title_descr_1}}</p>
+                        </div>
+                        <div class="badge-position">
+                            <ul v-for="(tag,j) in itemObj.tags" :key="j"> <a href="#" class="badge badge-pill badge-primary"> {{tag}} </a></ul>
+                        </div>
 
+                    </div>
+                </a>
 
-  <!-- Moadal show details -->
+                <!-- Moadal show details -->
 
+                <div class="modal fade bd-example-modal-lg" tabindex="-1 " role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl">
+                        <div class="modal-content">
+                            <DetailedItem></DetailedItem>
+                        </div>
+                    </div>
+                </div>
 
-<div class="modal fade bd-example-modal-lg" tabindex="-1 " role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-xl">
-    <div class="modal-content">
-      <DetailedItem ></DetailedItem>
+            </v-flex>
+        </div>
     </div>
-  </div>
-</div>
-
-
-
-      </div>
-    </div>
-  </div>
-
-
 
 </div>
-
-
-   
-
 </template>
 
-
 <script>
-  import {mapState} from 'vuex';
+import {
+    mapState
+} from 'vuex';
 
-  export default {
+export default {
     name: "CardItem",
     data: () => ({
-      cards: [
+        cards: [
 
-        { title: 'Proyecto 1', subtitle:'Subtitulo de proyecto' ,src: require('@/assets/Proy1.png'), tags:"Financiero"},
+            {
+                title: 'Proyecto 1',
+                subtitle: 'Subtitulo de proyecto',
+                src: require('@/assets/Proy1.png'),
+                tags: "Financiero"
+            },
 
-
-      ],
+        ],
     }),
-     components:{
-     DetailedItem: () => import('@/components/DetailsItem'),
+    components: {
+        DetailedItem: () => import('@/components/DetailsItem'),
     },
-     computed: mapState(['items']),
-     
-  
-  methods:{
-    displayDetails(itemId){
-     // this.$store.dispatch('load',itemId)
-     this.$store.dispatch('loadDetailsItem',itemId) 
+    computed: mapState(['items']),
+
+    methods: {
+        displayDetails(itemId) {
+            // this.$store.dispatch('load',itemId)
+            this.$store.dispatch('loadDetailsItem', itemId)
+        }
     }
-  }
-  }
-  
+}
 </script>
 
-
-<style >
-
-  .rowCards{
+<style>
+.rowCards {
     height: 500px;
-  }
+}
 
-  .card-group{
+.card-group {
     height: 500px;
-  }
-  .card{
+}
+
+.card {
     height: 500px;
-  }
+}
 
-  .card-title{
-    color:#ffffff;
-  }
+.card-title {
+    color: #ffffff;
+}
 
-  .card-text{
-    color:#ffffff;
-  }
+.card-text {
+    color: #ffffff;
+}
 
-
-  .card-img-overlay:hover{
+.card-img-overlay:hover {
     transition: 1s !important;
     transition-delay: 1s !important;
-    background-image: linear-gradient(to top, rgba(46,46,46,0), rgba(46,46,46,1));
-  }
+    background-image: linear-gradient(to top, rgba(46, 46, 46, 0), rgba(46, 46, 46, 1));
+}
 
-  .badge{
+.badge {
     margin: 1px;
     font-size: 130% !important;
-  }
+}
 
-  .badge-primary{
+.badge-primary {
     background-color: #5576d1 !important;
-  }
+}
 
-  .badge-primary:hover{
+.badge-primary:hover {
     background-color: #7e9df2 !important;
-  }
+}
 
-  .badge-light{
-    color:#5576d1 !important;
+.badge-light {
+    color: #5576d1 !important;
     border-color: #5576d1;
-  }
+}
 
-  .badge-position{
-      position: absolute;
-      left:0px;
-      bottom: 5px;
-      padding:5px 10px;
-  }
+.badge-position {
+    position: absolute;
+    left: 0px;
+    bottom: 5px;
+    padding: 5px 10px;
+}
 
-  .overlay{
-    
+.overlay {
+
     height: 100%;
     width: 100%;
     opacity: 0;
     transition: .2s ease;
-  }
+}
 
-  .card:hover .overlay{
+.card:hover .overlay {
     opacity: 1;
-  }
+}
 
-  .nopadding {
-     padding: 0 !important;
-     margin: 0 !important;
-  }
+.nopadding {
+    padding: 0 !important;
+    margin: 0 !important;
+}
 
-  .detailsItem{
+.detailsItem {
     position: absolute;
-  }
+}
 
-  ul, a{
-    display:inline;
-   
-  }
+ul,
+a {
+    display: inline;
 
-
-
+}
 </style>
